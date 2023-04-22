740448f9bcb2d670740beb76b17598a758d3451c
Jordan Hilado <jordanalihilado@gmail.com>
2023-04-22 00:52:27 -0700

    add landing page

--- src/pages/api/auth/[...auth0].ts
+++ src/pages/api/auth/[...auth0].ts
@@ -1,4 +1,4 @@
-import { handleAuth } from '@auth0/nextjs-auth0';
+import { handleAuth } from "@auth0/nextjs-auth0";
 
 /* This creates the following routes:
 /api/auth/login: The route used to perform login with Auth0.

4ea52213e2fe35c0c465fb63a800ca0038b572a3
Jordan Hilado <jordanalihilado@gmail.com>
2023-04-21 23:39:45 -0700

    add firebase

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAd9qt3Ogw4aaD4BGcUho_TJBilJPAOiUg",
+  authDomain: "foodle-ab6fe.firebaseapp.com",
+  projectId: "foodle-ab6fe",
+  storageBucket: "foodle-ab6fe.appspot.com",
+  messagingSenderId: "624393239781",
+  appId: "1:624393239781:web:f71bad911fabc2f890a80c",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

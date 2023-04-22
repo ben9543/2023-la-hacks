84648a3f37d837c4323f2e2e411e867945fc37f0
unknown <bennie.ben095@gmail.com>
2023-04-21 23:38:55 -0700

    Basic auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { UserProvider } from '@auth0/nextjs-auth0/client';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    // The authentication state exposed by UserProvider can be accessed in any component using the useUser() hook.
+    <UserProvider>
+      <Component {...pageProps} />
+    </UserProvider>
+  )
 }

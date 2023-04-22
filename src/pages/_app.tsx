d7a4ab624e75c003679870a9fbda743d80303beb
unknown <bennie.ben095@gmail.com>
2023-04-21 23:57:45 -0700

    Resolve merge conflict

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-=======
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
 import { UserProvider } from '@auth0/nextjs-auth0/client';
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -16,5 +9,4 @@ export default function App({ Component, pageProps }: AppProps) {
       <Component {...pageProps} />
     </UserProvider>
   )
->>>>>>> 84648a3f37d837c4323f2e2e411e867945fc37f0
 }

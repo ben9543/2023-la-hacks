5eefb618e210d0b0c024d29bf1e381cdf3a92893
pmpham <petermphamm@gmail.com>
2023-04-22 17:22:45 -0700

    added lookup

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -1,52 +1,100 @@
-import React from "react";
-import {
-  GoogleMap,
-  useJsApiLoader,
-  useLoadScript,
-} from "@react-google-maps/api";
-
 const containerStyle = {
-  width: "100vh",
-  height: "100vh",
+ width: "100%",
+ height: "100vh",
 };
 
-const center = {
-  lat: -3.745,
-  lng: -38.523,
-};
+import { useState, useMemo } from "react";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import usePlacesAutocomplete, {
+ getGeocode,
+ getLatLng,
+} from "use-places-autocomplete";
+import {
+ Combobox,
+ ComboboxInput,
+ ComboboxPopover,
+ ComboboxList,
+ ComboboxOption,
+} from "@reach/combobox";
+import "@reach/combobox/styles.css";
 
-function MyComponent() {
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string,
-  });
+export default function Places() {
+ const { isLoaded } = useLoadScript({
+  googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string,
+  libraries: ["places"],
+ });
 
-  const [map, setMap] = React.useState(null);
-
-  const onLoad = React.useCallback(function callback(map: any) {
-    // This is just an example of getting and using the map instance!!! don't just blindly copy!
-    const bounds = new window.google.maps.LatLngBounds(center);
-    map.fitBounds(bounds);
-
-    setMap(map);
-  }, []);
-
-  const onUnmount = React.useCallback(function callback(map: any) {
-    setMap(null);
-  }, []);
-
-  return isLoaded ? (
-    <GoogleMap
-      mapContainerStyle={containerStyle}
-      center={center}
-      zoom={10}
-      onLoad={onLoad}
-      onUnmount={onUnmount}
-    >
-      <></>
-    </GoogleMap>
-  ) : (
-    <></>
-  );
+ if (!isLoaded) return <div>Loading...</div>;
+ return <Map />;
 }
 
-export default React.memo(MyComponent);
+function Map() {
+ const center = useMemo(() => ({ lat: 43.45, lng: -80.49 }), []);
+ const [selected, setSelected] = useState(null);
+
+ return (
+  <>
+   <div className="places-container">
+    <PlacesAutocomplete setSelected={setSelected} />
+   </div>
+  </>
+ );
+}
+
+const PlacesAutocomplete = ({ setSelected }) => {
+ const {
+  ready,
+  value,
+  setValue,
+  suggestions: { status, data },
+  clearSuggestions,
+ } = usePlacesAutocomplete();
+
+ const handleSelect = async (address) => {
+  setValue(address, false);
+  clearSuggestions();
+  console.log(address);
+  const results = await getGeocode({ address });
+  const { lat, lng } = await getLatLng(results[0]);
+  setSelected({ lat, lng });
+ };
+
+ return (
+    <Combobox onSelect={handleSelect} className="bg-base-100 h-1/3">
+   <ComboboxInput
+    value={value}
+    onChange={(e) => setValue(e.target.value)}
+    disabled={!ready}
+    className="w-3/6 text-center m-auto flex "
+    placeholder="Wya neighbor"
+   />
+   <ComboboxPopover>
+    <ComboboxList>
+     {status === "OK" &&
+      data.map(({ place_id, description }) => (
+       <ComboboxOption key={place_id} value={description} className="color-black"/>
+      ))}
+    </ComboboxList>
+   </ComboboxPopover>
+  </Combobox>
+ );
+};
+
+
+  {/* <Combobox onSelect={handleSelect} >
+   <ComboboxInput
+    value={value}
+    onChange={(e) => setValue(e.target.value)}
+    disabled={!ready}
+    className="w-3/6 text-center m-auto flex"
+    placeholder="Search an address"
+   />
+   <ComboboxPopover>
+    <ComboboxList>
+     {status === "OK" &&
+      data.map(({ place_id, description }) => (
+       <ComboboxOption key={place_id} value={description} />
+      ))}
+    </ComboboxList>
+   </ComboboxPopover>
+  </Combobox>*/}
